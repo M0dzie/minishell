e0b070a6bceed0e2bf44029be16f34ce9265629e
Modzie <modzielol@gmail.com>
2023-01-30 11:01:25 +0100

    print_pwd

--- main.c
+++ main.c
@@ -6,11 +6,32 @@
 /*   By: thmeyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 15:38:17 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/01/30 09:37:46 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/01/30 11:01:04 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	print_pwd(void)
+{
+	char	pwd[1024];
+
+	getcwd(pwd, sizeof(pwd));
+	printf("%s\n", pwd);
+}
+
+// void	print_env()
+// {
+// 	getenv();
+// }
+
+// char	*read_prompt(char *str)
+// {
+// 	char	*line;
+
+// 	line = readline();
+// 	return (line);
+// }
 
 int	main(int argc, char **argv)
 {
@@ -20,9 +41,14 @@ int	main(int argc, char **argv)
 		return (ft_putendl_fd("Error\nminishell doesn't take any parameters.", \
 		2), 0);
 	(void) argv;
+	// while (1)
+	// {
 	pid = fork();
 	if (pid < 0)
 		return (ft_putendl_fd("Error\nNo child process was created.", 2), 0);
 	printf("pid = %d\n", pid);
+	print_pwd();
+	wait(NULL);
+	// }
 	return (0);
 }
