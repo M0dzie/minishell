57939cff72e57aa2a201112f72638daf56e439f9
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-02-07 09:40:27 +0100

    put color to exec

--- main.c
+++ main.c
@@ -6,45 +6,56 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 15:38:17 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/02/06 18:29:21 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/02/07 09:39:38 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	print_pwd(void)
+void	print_tamer(int sort)
 {
-	char	pwd[1024];
+	char	*output;
 
-	getcwd(pwd, sizeof(pwd));
-	printf("%s\n", pwd);
+	if (sort == 1)
+	{
+		output = getenv("PWD");
+		printf("%s\n", output);
+	}
 }
 
-// void	print_env()
-// {
-// 	getenv();
-// }
-
-void	read_prompt(char *input)
+void	print_env(char **env)
 {
-	ft_printf("input = %s\n", input);
+	int	i;
+
+	i = -1;
+	while (env[++i])
+		printf("%s\n",env[i]);
 }
 
-int	main(int argc, char **argv)
+void	read_prompt(char *input, char **env)
+{
+	if (ft_strncmp(input, "pwd", 3) == 0)
+		print_tamer(1);
+	else if (ft_strncmp(input, "env", 3) == 0)
+		print_env(env);
+	else
+		ft_printf("input = %s\n", input);
+}
+
+int	main(int argc, char **argv, char **env)
 {
 	pid_t	pid;
 	char	*input;
 
-	(void) input;
 	(void) argv;
 	if (argc != 1)
 		return (ft_putendl_fd("Error\nminishell doesn't take any parameters.", \
 		2), 0);
 	while (1)
 	{
-		input = readline("$> ");
+		input = readline("\033[1;36mminishell \033[1;34m$> \033[0m");
 		add_history(input);
-		read_prompt(input);
+		read_prompt(input, env);
 	}
 	printf("pid = %d\n", pid);
 	return (0);
