11c1491df9def916ffbc40b9bc4415305fd3f793
Mehdi Sapin <mehdisapin@pop-os.localdomain>
2023-02-14 22:06:51 +0100

    Able to detect builtins and handle some of them (cd, pwd, exit). Can process other command. Need to be able to handle many command through pipe. Now can execute the first one with arguments

--- sources/execution/execution.c
+++ sources/execution/execution.c
@@ -6,11 +6,13 @@
 /*   By: mehdisapin <mehdisapin@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 22:49:25 by mehdisapin        #+#    #+#             */
-/*   Updated: 2023/02/14 18:18:15 by mehdisapin       ###   ########.fr       */
+/*   Updated: 2023/02/14 22:01:24 by mehdisapin       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+#include <sys/types.h>
+#include <sys/wait.h>
 
 char	*get_cmd_path(char *cmd, char **envp)
 {
@@ -44,7 +46,7 @@ void	execute_cmd(char **cmd_args, char **envp)
 	execve(cmd_path, cmd_args, envp);
 	free(cmd_path);
 	ft_arrfree(cmd_args);
-	exit(0);
+	// exit(0);
 }
 
 void	create_pipe(char **args_cmd, t_msl *ms, char **envp)
@@ -61,28 +63,133 @@ void	create_pipe(char **args_cmd, t_msl *ms, char **envp)
 		wait(NULL);
 }
 
+int	strict_cmp(const char *builts, const char *cmd)
+{
+	size_t	i;
+	size_t	cmd_len;
+
+	i = -1;
+	cmd_len = ft_strlen(cmd);
+	if (ft_strlen(builts) != ft_strlen(cmd))
+		return (0);
+	while (++i < cmd_len)
+	{
+		if (builts[i] != cmd[i])
+			return (0);
+	}
+	return (1);
+}
+
+int	is_builtins(char *cmd)
+{
+	char	*tmp_cmd;
+	char	**tmp_split;
+
+	if (ft_strchr(cmd, '/'))
+	{
+		tmp_split = ft_split(cmd, '/');
+		tmp_cmd = tmp_split[ft_arrlen(tmp_split) - 1];
+	}
+	else
+		tmp_cmd = ft_strdup(cmd);
+	if (strict_cmp("echo", tmp_cmd) | strict_cmp("cd", tmp_cmd) | 
+		strict_cmp("pwd", tmp_cmd) | strict_cmp("export", tmp_cmd) |
+		strict_cmp("unset", tmp_cmd) | strict_cmp("env", tmp_cmd) |
+		strict_cmp("exit", tmp_cmd))
+		return (1);
+	return (0);
+}
+
+void	builtins_execution(t_msl *ms, char **envp, char **args_cmd)
+{
+	(void)ms;
+	(void)envp;
+
+	char	*tmp_path;
+	char	**tmp_args;
+
+	tmp_path = get_cmd_path(args_cmd[0], envp);
+	tmp_args = ft_calloc(1, sizeof(char *));
+	tmp_args = ft_arradd_back(tmp_args, tmp_path);
+
+	// ft_printf("%s\n", tmp_args[0]);
+
+	// ft_printf("Builtins function : %s\n", tmp_path[0]);
+	if (strict_cmp("echo", args_cmd[0]))
+		ft_printf("echo execution\n");
+	else if (strict_cmp("cd", args_cmd[0]))
+	{
+		if (ft_arrlen(args_cmd) > 2)
+			ft_printf("bash: cd: too many arguments\n");
+		else if (ft_arrlen(args_cmd) == 2)
+		{
+			// test if valid folder
+			if (access(args_cmd[1], X_OK) == 0)
+			{
+				// if no other cmd change directory
+				if (ms->c_pipe == 0)
+					chdir(args_cmd[1]);
+			}
+			else
+				ft_printf("bash: cd: %s: No such file or directory\n", args_cmd[1]);
+		}
+		else
+		{
+			// if only cd check if no pipe to change directory
+			if (ms->c_pipe == 0)
+				chdir(getenv("HOME"));
+				// change to root dir
+		}
+	}
+	else if (strict_cmp("pwd", args_cmd[0]))
+	{
+		tmp_args = ft_arradd_back(tmp_args, tmp_path);
+		execve(tmp_path, tmp_args, NULL);
+	}
+	else if (strict_cmp("export", args_cmd[0]))
+		ft_printf("export execution\n");
+	else if (strict_cmp("unset", args_cmd[0]))
+		ft_printf("unset execution\n");
+	else if (strict_cmp("env", args_cmd[0]))
+		ft_printf("env execution\n");
+	else if (strict_cmp("exit", args_cmd[0]))
+	{
+		if (ms->c_pipe == 0)
+			exit (0);
+	}
+}
+
+void	standard_execution(t_msl *ms, char **envp, char **args_cmd)
+{
+	(void)ms;
+	(void)envp;
+	ft_printf("Classic function : %s\n", args_cmd[0]);
+	create_pipe(args_cmd, ms, envp);
+}
+
 void	execution(t_msl *ms, char *input, char **envp)
 {
-	(void)input;
-
 	char	*input_test = "/usr/bin/ls \"-l\" '-a'";
-	char	*args_cmd[] = {"/usr/bin/ls", "\"-l\"", "'-a'", NULL};
+	char	*args_cmd[] = {"/usr/bin/ls", "'-'l", "-a", NULL};
 	char	*input_echo = "echo \"phrase\" 'a la con'";
 	char	*arr_echo[] = {"/usr/bin/echo", "'~' une phra\"se \"exemple avec \
 'un vrai $PWD' qui ne s\"'\"affichera qu\"'\"entre double quote \"$PWD\"", "'", NULL};
 
+	char	***cmds;
+	char	**tmp_split;
+	int	i;
+
+	ms->c_pipe = count_pipes(input);
+	ms->cmds = ft_calloc(ms->c_pipe + 1, sizeof(char **));
+	ms->split = ft_split(input, '|');
+	i = -1;
+	while (ms->split[++i])
+		ms->cmds[i] = ft_split(ms->split[i], ' ');
 	for (int i = 0; i <= ms->c_pipe; i++)
 	{
-		ft_printf("command %d : %s\n", i, ms->cmds[i][0]);
-		// create_pipe(ms->cmds[i], ms, envp);
-		for (int j = 1; ms->cmds[i][j]; j++)
-			ft_printf("argument %d : %s\n", j, ms->cmds[i][j]);
+		if (is_builtins(ms->cmds[i][0]))
+			builtins_execution(ms, envp, ms->cmds[i]);
+		else
+			standard_execution(ms, envp, ms->cmds[i]);
 	}
-	// ft_printf("input : %s\n", ms->cmds[0][1]);
-
-	// execute_cmd(args_cmd, &ms);
-	// execute_cmd(ft_split(input_test, ' '), &ms);
-	// execute_cmd(arr_echo, ms, envp);
-
-	// create_pipe(arr_echo, ms, envp);
 }
