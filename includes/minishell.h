a3c63494aa248347b50d59a7f696cac366529298
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-02-14 16:03:35 +0100

    standby parsing, need a reset

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: thmeyer < thmeyer@student.42lyon.fr >      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 15:23:06 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/02/14 14:21:40 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/02/14 15:56:21 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,8 @@ typedef struct s_msl
 	int		c_pipe;
 }				t_msl;
 
-int		checking_opened_quotes(char *input, char c);
+int		check_opened_quotes(char *input, char c);
+int		check_wrong_quotes_echo(char *input);
 int		count_pipes(char *input);
 int		ms_strcmp(char *s1, char *s2);
 int		parsing_errors(char *input);
--- sources/check_errors/checking_errors.c
+++ sources/check_errors/checking_errors.c
@@ -6,13 +6,13 @@
 /*   By: thmeyer < thmeyer@student.42lyon.fr >      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 11:16:57 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/02/14 14:22:19 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/02/14 15:59:20 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int	checking_opened_quotes(char *input, char c)
+int	check_opened_quotes(char *input, char c)
 {
 	int	i;
 	int	quote;
@@ -35,7 +35,7 @@ int	checking_opened_quotes(char *input, char c)
 	return (0);
 }
 
-int	parsing_errors(char *input)
+int	check_wrong_quotes_echo(char *input)
 {
 	int	i;
 
@@ -43,8 +43,27 @@ int	parsing_errors(char *input)
 	while (input[++i])
 	{
 		if (input[i] == 34 || input[i] == 39)
+			return (check_opened_quotes(input, input[i]), -1);
+	}
+	return (0);
+}
+
+int	parsing_errors(char *input)
+{
+	int	i;
+
+	i = -1;
+	if (ft_strncmp(input, "echo", 4) == 0)
+	{
+		if (check_wrong_quotes_echo(input) == -1)
+			return (-1);
+		return (0);
+	}
+	while (input[++i])
+	{
+		if (input[i] == 34 || input[i] == 39)
 		{
-			if (checking_opened_quotes(input, input[i]))
+			if (check_opened_quotes(input, input[i]))
 				return (-1);
 		}
 		if (input[i] == ';')
