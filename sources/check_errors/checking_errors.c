d0df708b4239f0e881f431d2b9480c872e1bc3d9
Thomas Meyer <thomasmeyer@MacBook-Pro-de-Thomas.local>
2023-02-17 12:08:39 +0100

    error '&' done

--- sources/check_errors/checking_errors.c
+++ sources/check_errors/checking_errors.c
@@ -6,7 +6,7 @@
 /*   By: thmeyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 11:16:57 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/02/17 12:00:21 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/02/17 12:07:38 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,10 @@ int	display_errors(t_msl *ms, int type)
 		ft_putendl_fd("syntax error near unexpected token '|'", 2);
 	if (type == '|' && ms->input[1] == '|')
 		ft_putendl_fd("syntax error near unexpected token '||'", 2);
+	if (type == '&' && (ms->input[1] == ' ' || ms->input[1] == '\0'))
+		ft_putendl_fd("syntax error near unexpected token '&'", 2);
+	if (type == '&' && ms->input[1] == '&')
+		ft_putendl_fd("syntax error near unexpected token '&&'", 2);
 	return (-1);
 }
 
@@ -61,7 +65,7 @@ int	parsing_errors(t_msl *ms)
 	if (ms->input[0] == '/' || ms->input[0] == '\\' || ms->input[0] == '-' \
 	|| ms->input[0] == ' ')
 		return (display_errors_pipe(ms, ms->input[0]));
-	if (ms->input[0] == '|')
+	if (ms->input[0] == '|' || ms->input[0] == '&')
 		return (display_errors(ms, ms->input[0]));
 	if (ms->input[0] == '!' || ms->input[0] == ':' || ms->input[0] == '\t' \
 	|| ms->input[0] == '#')
