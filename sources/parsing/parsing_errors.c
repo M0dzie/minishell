b1658cf8aaf1ef15532e85b5711bfa4e13635ec4
Thomas Meyer <thomasmeyer@MacBook-Pro-de-Thomas.local>
2023-02-17 15:08:38 +0100

    reorganized parsing dir

--- sources/parsing/parsing_errors.c
+++ sources/parsing/parsing_errors.c
@@ -6,7 +6,7 @@
 /*   By: thmeyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 11:16:57 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/02/17 14:57:03 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/02/17 15:05:31 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,45 +35,6 @@ int	display_errors(t_msl *ms, int type)
 	|| ms->input[1] == ')' || ms->input[1] == '('))
 		ft_putendl_fd("syntax error near unexpected token ')'", 2);
 	return (-1);
-}
-
-int	display_errors_redirect(t_msl *ms, int type)
-{
-	int	i;
-
-	i = -1;
-	ft_putstr_fd("minishell: ", 2);
-	while (ms->input[++i])
-	{
-		if (ms->input[i] == '|')
-			return (ft_putendl_fd("syntax error near unexpected token '|'", \
-			2), -1);	
-	}
-	if (ms->input[0] == '<' && ms->input[1] == '<' && ms->input[2] == '<')
-		return (ft_putendl_fd("syntax error near unexpected token '<<'", 2), -1);
-	if (ms->input[0] == '>' && ms->input[1] == '>' && ms->input[2] == '>')
-		return (ft_putendl_fd("syntax error near unexpected token '>>'", 2), -1);
-	if (ms->input[1] == '\0' || (ms->input[1] == type && \
-	ms->input[2] == '\0') || (ms->input[1] == '<' && ms->input[2] == '\0') || \
-	(ms->input[1] == '>' && ms->input[2] == '\0'))
-		return (ft_putendl_fd("syntax error near unexpected token 'newline'", \
-		2), -1);
-	return (display_errors_redirect2(ms, type));
-}
-
-int	display_errors_redirect2(t_msl *ms, int type)
-{
-	if (type == '>' && ms->input[1] == ' ' && ms->input[2] == '>')
-		return (ft_putendl_fd("syntax error near unexpected token '>'", 2), -1);
-	if (type == '<' && ms->input[1] == ' ' && ms->input[2] == '<')
-		return (ft_putendl_fd("syntax error near unexpected token '<'", 2), -1);
-	if (type == '<' && ms->input[1] == '<' && ms->input[2] == ' ' && \
-	ms->input[3] == '<')
-		return (ft_putendl_fd("syntax error near unexpected token '<<'", 2), -1);
-	if (type == '>' && ms->input[1] == '>' && ms->input[2] == ' ' && \
-	ms->input[3] == '>')
-		return (ft_putendl_fd("syntax error near unexpected token '>>'", 2), -1);
-	return (0);
 }
 
 int	display_errors_pipe(t_msl *ms, int type)
