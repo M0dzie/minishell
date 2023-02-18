226c1c9c207ed6dafc875ee5a497e6f216030523
Modzie <modzielol@gmail.com>
2023-02-18 12:07:20 +0100

    fixing issues with <<  >>

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: thmeyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 15:23:06 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/02/18 10:33:20 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/02/18 11:55:00 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,7 @@ int		display_errors_pipe(t_msl *ms, int type);
 int		display_errors_redirect(t_msl *ms, int type);
 int		display_errors_redirect2(t_msl *ms, int type);
 int		display_errors_redirect3(t_msl *ms, int type);
+int		display_errors_redirect4(t_msl *ms, int type);
 int		ms_strcmp(char *s1, char *s2);
 int		parsing_errors(t_msl *ms);
 int		parsing_quotes(t_msl *ms);
