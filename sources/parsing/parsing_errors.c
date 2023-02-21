9709dd09e2c826b0ae362d12f7f53fb69fcb1912
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-02-21 18:18:50 +0100

    parsing ; errors

--- sources/parsing/parsing_errors.c
+++ sources/parsing/parsing_errors.c
@@ -6,7 +6,7 @@
 /*   By: thmeyer < thmeyer@student.42lyon.fr >      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 11:16:57 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/02/21 18:03:19 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/02/21 18:17:18 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,6 +77,21 @@ int	parsing_pipes_input(t_msl *ms)
 	return (0);
 }
 
+int	parsing_semicolon(t_msl *ms)
+{
+	int	i;
+
+	i = -1;
+	while (ms->input[++i])
+	{
+		if (ms->input[i] == ';' && (ft_isascii(ms->input[i + 1]) || \
+		!ms->input[i + 1]))
+			return (ft_putendl_fd("syntax error near unexpected token ';'", \
+			2), -1);
+	}
+	return (0);
+}
+
 int	parsing_errors(t_msl *ms)
 {
 	if (ms->input[0] == '/' && (ms->input[1] == '.' || \
@@ -92,5 +107,7 @@ int	parsing_errors(t_msl *ms)
 		return (-1);
 	if (ms->c_pipe > 0)
 		return (parsing_pipes_input(ms));
+	if (parsing_semicolon(ms) == -1)
+		return (-1);
 	return (0);
 }
