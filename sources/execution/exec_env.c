d991550b8c9f67567556b213a4f9c237554bb423
Mehdi Sapin <mehdisapin@pop-os.localdomain>
2023-02-22 22:16:28 +0100

    Finished execution of env, need work on the exit number return

--- sources/execution/exec_env.c
+++ sources/execution/exec_env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec_env.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msapin <msapin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mehdisapin <mehdisapin@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 09:38:38 by mehdisapin        #+#    #+#             */
-/*   Updated: 2023/02/22 18:57:51 by msapin           ###   ########.fr       */
+/*   Updated: 2023/02/22 22:12:00 by mehdisapin       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,43 +37,34 @@ int	first_invalid(char *arg)
 	return (0);
 }
 
-int	cmd_valid(char **arg)
+int	cmd_valid(t_msl *ms, char **arg)
 {
-	printf("%s\n", arg[0]);
+	char	**env;
 	char	**tmp_paths;
 	char	*join_one;
 	char	*join_two;
 	int		i;
 
 	i = -1;
+	env = ft_getenv(ms);
 	tmp_paths = ft_split(getenv("PATH"), ':');
 	while (tmp_paths[++i])
 	{
 		join_one = ft_strjoin(tmp_paths[i], "/");
 		join_two = ft_strjoin(join_one, arg[0]);
 		if (access(join_two, F_OK) == 0)
-			return (printf("cmd valid\n"), 1);			/// execute command
+			return (pipe_one(arg, ms, env), 1);
 		free(join_one);
 		free(join_two);
 	}
-	return (printf("cmd invalid\n"), ft_arrfree(tmp_paths), 0);
+	return (ft_arrfree(tmp_paths), 0);
 }
 
 void	exec_env(t_msl *ms, char **args_cmd, char **envp)
 {	
-	// int	i;
-
-	// i = -1;
-	// while (envp[++i])
-	// 	printf("%s\n", envp[i]);
-	// printf("\n");
-	// create_pipe(args_cmd, ms, envp);
-	// handle_cmd(ms, args_cmd, envp);
-	int	valid;
 	int	nb_args;
 	int	i;
 
-	valid = 1;
 	nb_args = ft_arrlen(args_cmd);
 	if (nb_args > 1)
 	{
@@ -84,19 +75,15 @@ void	exec_env(t_msl *ms, char **args_cmd, char **envp)
 			{
 				if (first_invalid(args_cmd[i]))
 					return ;
-				else if (cmd_valid(args_cmd + i))
+				else if (cmd_valid(ms, args_cmd + i))
 					return ;
 				else
 				{
 					display_error_exec("env: ", args_cmd[i], 2);
 					return ;
 				}
-				// printf("%s is not env\n", args_cmd[i]);
-				// valid = 0;
 			}
 		}
-		// printf("multiple args\n");
 	}
-	if (valid)
-		display_env(ms);
+	display_env(ms);
 }
