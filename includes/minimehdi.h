374c7ce77851d3fa550a31ea374a689e7372c068
Mehdi Sapin <msapin@student.42lyon.fr>
2023-02-23 14:08:38 +0100

    Added getvar

--- includes/minimehdi.h
+++ includes/minimehdi.h
@@ -6,7 +6,7 @@
 /*   By: msapin <msapin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 17:05:37 by msapin            #+#    #+#             */
-/*   Updated: 2023/02/23 11:09:34 by msapin           ###   ########.fr       */
+/*   Updated: 2023/02/23 14:07:25 by msapin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@ void	display_env(t_msl *ms);
 char	**ft_getenv(t_msl *ms);
 void	pipe_one(char **args_cmd, t_msl *ms, char **envp);
 int		strict_cmp(const char *builts, const char *cmd);
+t_var	*getvar(t_msl *ms, char *name);
 
 #endif
