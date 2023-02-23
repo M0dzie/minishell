278d2fd2396cf9daea4faf6de37caa9f31a91499
Mehdi Sapin <msapin@student.42lyon.fr>
2023-02-23 14:02:13 +0100

    Modified prototype

--- includes/minimehdi.h
+++ includes/minimehdi.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minimehdi.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mehdisapin <mehdisapin@student.42.fr>      +#+  +:+       +#+        */
+/*   By: msapin <msapin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 17:05:37 by msapin            #+#    #+#             */
-/*   Updated: 2023/02/22 22:12:57 by mehdisapin       ###   ########.fr       */
+/*   Updated: 2023/02/23 11:09:34 by msapin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@
 void	display_env(t_msl *ms);
 char	**ft_getenv(t_msl *ms);
 void	pipe_one(char **args_cmd, t_msl *ms, char **envp);
+int		strict_cmp(const char *builts, const char *cmd);
 
 #endif
