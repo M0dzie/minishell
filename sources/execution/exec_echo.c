121630b44999739c44708500bb3d8ec10901c002
Mehdi Sapin <mehdisapin@pop-os.localdomain>
2023-02-27 12:08:44 +0100

    Fixed echo alone segfaulting, able to print exit status number by using echo 0

--- sources/execution/exec_echo.c
+++ sources/execution/exec_echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec_echo.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msapin <msapin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mehdisapin <mehdisapin@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 09:41:27 by mehdisapin        #+#    #+#             */
-/*   Updated: 2023/02/23 13:32:41 by msapin           ###   ########.fr       */
+/*   Updated: 2023/02/27 12:07:09 by mehdisapin       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,6 @@
 
 void	exec_echo(t_msl *ms, char **args_cmd, char **envp)
 {
-	// if $? in parsing change this with ms->rtn_int
-	// char	*tmp_arg[] = {"echo", ft_itoa(ms->rtn_int), NULL};
-	// create_pipe(tmp_arg, ms, envp);
-
-	// printf("Errno : %d\n", errno);
-	
-	// create_pipe(args_cmd, ms, envp);
 	if (strict_cmp(args_cmd[1], "$?"))
 		printf("%d\n", ms->status);
 	else
