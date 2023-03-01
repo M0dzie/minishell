2f03b58f71f9d414027bc2b5b6ba2359217a03be
Modzie <modzielol@gmail.com>
2023-03-01 17:43:15 +0100

    rename file for better comprehension

--- sources/parsing/parsing.c
+++ sources/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: thmeyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 17:49:26 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/03/01 17:17:50 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/03/01 17:31:13 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,59 @@ void	count_pipes(t_msl *ms)
 	ms->c_cmd = ms->c_pipe + 1;
 }
 
+int is_space(char c)
+{
+    return (c == ' ' || c == '\t' || c == '\n');
+}
+
+char **my_split(char *str)
+{
+    int i, j, k, in_quote;
+    char **result;
+    
+    result = (char **)malloc(sizeof(char *) * 256);
+    if (!result)
+        return (NULL);
+    i = 0;
+    j = 0;
+    k = 0;
+    in_quote = 0;
+    while (str[i])
+    {
+        if (str[i] == '"' && (i == 0 || str[i - 1] != '\\'))
+            in_quote = !in_quote;
+        if (!in_quote && is_space(str[i]))
+        {
+            if (k > 0)
+            {
+                result[j] = (char *)malloc(sizeof(char) * (k + 1));
+                if (!result[j])
+                    return (NULL);
+                for (int l = 0; l < k; l++)
+                    result[j][l] = str[i - k + l];
+                result[j][k] = '\0';
+                j++;
+                k = 0;
+            }
+        }
+        else
+            k++;
+        i++;
+    }
+    if (k > 0)
+    {
+        result[j] = (char *)malloc(sizeof(char) * (k + 1));
+        if (!result[j])
+            return (NULL);
+        for (int l = 0; l < k; l++)
+            result[j][l] = str[i - k + l];
+        result[j][k] = '\0';
+        j++;
+    }
+    result[j] = NULL;
+    return (result);
+}
+
 void	read_prompt(t_msl *ms, char **envp)
 {
 	int		i;
@@ -60,7 +113,7 @@ void	read_prompt(t_msl *ms, char **envp)
 		ms->split = ft_split(ms->input, '|');
 		if (!ms->split)
 			return (free(ms->input));
-		ms->cmds[i] = ft_split(ms->split[i], ' ');
+		ms->cmds[i] = my_split(ms->split[i]);
 		if (!ms->cmds)
 			return (free(ms->input));
 	}
