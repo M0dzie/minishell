40c2c1cd7c497b4b0e12658d38a0dc0f1008c602
Modzie <modzielol@gmail.com>
2023-03-07 17:46:42 +0100

    need a recursive and need a strncmp to take value in env ?

--- sources/env_variables/switch_var.c
+++ sources/env_variables/switch_var.c
@@ -6,7 +6,7 @@
 /*   By: thmeyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 09:35:45 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/03/07 17:29:24 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/03/07 17:44:36 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,16 +25,16 @@ int	check_sign(char *split, int i)
 
 char	*get_value(t_msl *ms, char *split)
 {
-	if (split[0] == ' ' || !split[0])
-		return ("$");
-	if (split[0] == '?')
-		return (ft_itoa(ms->status));
 	split = ft_strtrim(split, "$ \'\"");
 	if (!split)
 		return (NULL);
-	// je dois penser a garder que la variables STRICTE donc refaire un check
-	// $, le seul delim qui reste
 	printf("clean var = %s\n", split);
+	if (!split[0])
+		return ("$");
+	if (split[0] == '?')
+		return (ft_itoa(ms->status));
+	// je dois penser a garder que la variables STRICTE donc refaire un check
+	// $, le seul delim qui reste -> strncmp ?
 	return ("Salut");
 }
 
